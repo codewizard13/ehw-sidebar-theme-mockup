5a768dcc2ed77ec6addf3fc39de7098348a6c694
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-03 09:52:37 -0600

    UPDATE main-content.php:  ADD flex-row of vid thumbs below top thumb

--- inc/main-content.php
+++ inc/main-content.php
@@ -1,4 +1,23 @@
 <h3><?php echo basename(__FILE__) ?></h3>
 
-<article class="thumb">
-</article>
+<?php include 'parts/tp-vid-thumb.php'; ?>
+
+
+<style>
+.flex-row {
+  display: flex;
+}
+.flex-row article {
+  flex: 1;
+  flex-direction: row;
+}
+</style>
+<section class="flex-row">
+
+<?php
+for ($i = 0; $i < 4; $i++) {
+  include 'parts/tp-vid-thumb.php';
+}
+?>
+
+</section>
