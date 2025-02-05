e394869353d036e97d56b0c0877070b601facb6d
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-05 06:55:20 -0600

    UPDATE main-content.php:  WRAP grid-container in video-grid

--- inc/main-content.php
+++ inc/main-content.php
@@ -3,14 +3,19 @@
 <?php include 'parts/tp-vid-thumb.php'; ?>
 
 
-<section class="grid-container">
+<section class="video-grid">
+
+  <div class="grid-container">
+
+    <?php
+    // Mockup 4 dummy vid posts
+    for ($i = 0; $i < 12; $i++) {
+      include 'parts/tp-vid-thumb.php';
+    }
+    ?>
 
 
-  <?php
-  // Mockup 4 dummy vid posts
-  for ($i = 0; $i < 12; $i++) {
-    include 'parts/tp-vid-thumb.php';
-  }
-  ?>
+  </div><!-- END grid-container -->
 
-</section>
+
+</section><!-- END video-grid -->
