5c28ee67b00a3dacc09214c31a23b585bff62f7f
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-06 16:20:41 -0600

    BROKEN: Try Flexbox wrap for vid-meta

--- inc/parts/tp-vid-meta.php
+++ inc/parts/tp-vid-meta.php
@@ -1,22 +1,21 @@
 <div class="vid-meta">
 
-  <div class="flex-group">
+  <!-- Time Meta (Fixed Width) -->
+  <div class="meta-item time-meta">
+    <i style="font-weight:bold; color:var(--color-secondary);" class="bi bi-calendar3"></i>
+    <span class="prop-label ico ico-cal-1"></span>
+    <span><time>February 5, 2025</time></span>
+  </div><!-- END time-meta -->
 
-    <div class="meta-item">
-      <i style="font-weight:bold; color:var(--color-secondary);" class="bi bi-calendar3"></i>
-      <span class="prop-label ico ico-cal-1"></span>
-      <span><time>February 5, 2025</time></span>
-    </div><!-- END meta-item -->
-
+  <!-- Meta Group (Guests and Hosts) -->
+  <div class="meta-group">
     <div class="meta-item">
       <span class="prop-label">
         <h4>Featured Guests:</h4>
       </span>
       <div class="comma-sep-list">
         <span>
-          <h4>
-            <a href="https://elijahstreams.com/guests/nathan-french/">Nathan French</a>
-          </h4>
+          <h4><a href="https://elijahstreams.com/guests/nathan-french/">Nathan French</a></h4>
         </span>
         <span>
           <h4><a href="#">Loren Ipsum2</a></h4>
@@ -36,26 +35,18 @@
       </span>
       <div class="comma-sep-list">
         <span>
-        <h4>
-          <a href="https://elijahstreams.com/guests/steve-shultz/">Steve Shultz</a>
-        </h4>
+          <h4><a href="https://elijahstreams.com/guests/steve-shultz/">Steve Shultz</a></h4>
         </span>
         <span>
           <h4><a href="#">Johnny Enlow</a></h4>
         </span>
       </div><!-- END comma-sep-list -->
     </div><!-- END meta-item -->
+  </div><!-- END meta-group -->
 
-
-
-  </div><!-- END flex-group -->
-
-  <div class="flex-group">
-
-    <div class="meta-item">
-      <button class="btn-small btn-share">Share</button>
-    </div><!-- END meta-item -->
-
-  </div><!-- END flex-group -->
+  <!-- Share Button (Fixed Size) -->
+  <div class="meta-item">
+    <button class="btn-small btn-share">Share</button>
+  </div><!-- END meta-item -->
 
 </div><!-- END vid-meta -->
