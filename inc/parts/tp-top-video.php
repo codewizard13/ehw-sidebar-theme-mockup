02d071b6ef8aef9cb63737a733c7c8b274f1496a
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-06 06:54:59 -0600

    ADD vid-meta to tp-top-video

--- inc/parts/tp-top-video.php
+++ inc/parts/tp-top-video.php
@@ -1,5 +1,51 @@
 <?php $dummy_vid_title = "Johnny Enlow Unfiltered 138" ?>
 
-<?php include ('tp-vid-thumb.php') ?>
+<?php include('tp-vid-thumb.php') ?>
 
 <h2 class="vid-title"><?php echo $dummy_vid_title ?></h2>
+
+
+<!-- POST META -->
+
+<div class="vid-meta">
+
+  <div class="flex-group">
+
+    <div class="meta-item">
+      <span class="prop-label">📅</span>
+      <span><time>February 5, 2025</time></span>
+    </div><!-- END meta-item -->
+
+    <div class="meta-item">
+    <span class="prop-label">
+        <h4>Featured Guests:</h4>
+      </span>
+      <span>
+        <h4>
+          <a href="https://elijahstreams.com/guests/nathan-french/">Nathan French</a>
+        </h4>
+      </span>
+    </div><!-- END meta-item -->
+
+    <div class="meta-item">
+    <span class="prop-label">
+        <h4>Hosts:</h4>
+      </span>
+      <span>
+        <h4>
+          <a href="https://elijahstreams.com/guests/steve-shultz/">Steve Shultz</a>
+        </h4>
+      </span>
+    </div><!-- END meta-item -->
+
+  </div><!-- END flex-group -->
+
+  <div class="flex-group">
+
+    <div class="meta-item">
+      <button class="btn-small btn-share">Share</button>
+    </div><!-- END meta-item -->
+
+  </div><!-- END flex-group -->
+
+</div><!-- END vid-meta -->
