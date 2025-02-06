5131f28bc8404ebe89faa0814fc5da947aa57267
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-06 15:19:59 -0600

    ADD style comma-sep-list

--- inc/parts/tp-top-video.php
+++ inc/parts/tp-top-video.php
@@ -12,24 +12,36 @@
   <div class="flex-group">
 
     <div class="meta-item">
-    <i style="font-weight:bold; color:var(--color-secondary);" class="bi bi-calendar3"></i>
-      <span class="prop-label">📅</span>
+      <i style="font-weight:bold; color:var(--color-secondary);" class="bi bi-calendar3"></i>
+      <span class="prop-label">📅🗓️</span>
       <span><time>February 5, 2025</time></span>
     </div><!-- END meta-item -->
 
     <div class="meta-item">
-    <span class="prop-label">
+      <span class="prop-label">
         <h4>Featured Guests:</h4>
       </span>
-      <span>
-        <h4>
-          <a href="https://elijahstreams.com/guests/nathan-french/">Nathan French</a>
-        </h4>
-      </span>
+      <div class="comma-sep-list">
+        <span>
+          <h4>
+            <a href="https://elijahstreams.com/guests/nathan-french/">Nathan French</a>
+          </h4>
+        </span>
+        <span>
+          <h4><a href="#">Loren Ipsum2</a></h4>
+        </span>
+        <span>
+          <h4><a href="#">Judge Michael Brown</a></h4>
+        </span>
+        <span>
+          <h4><a href="#">Candy Christmas</a></h4>
+        </span>
+
+      </div><!-- END comma-sep-list -->
     </div><!-- END meta-item -->
 
     <div class="meta-item">
-    <span class="prop-label">
+      <span class="prop-label">
         <h4>Hosts:</h4>
       </span>
       <span>
