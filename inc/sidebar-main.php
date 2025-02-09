c70c448d9012b60cdb75d34a8698b07bf388f148
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-09 17:43:04 -0600

    UPDATE inc/sidebar-main.php:  MOVE sidebar content from index.php

--- inc/sidebar-main.php
+++ inc/sidebar-main.php
@@ -1,21 +1,32 @@
-<h3><?php //echo basename(__FILE__) ?></h3>
+<!-- Sidebar / Ads -->
 
-<?php include 'parts/tp-vid-thumb.php'; ?>
+<aside class="sidebar">
 
+  <?php include('inc/parts/tp-ad-banner-1.php') ?>
 
-<section class="video-grid">
-
-  <div class="grid-container">
-
-    <?php
-    // Mockup 4 dummy vid posts
-    for ($i = 0; $i < 5; $i++) {
-      include 'parts/tp-vid-thumb.php';
-    }
-    ?>
+  <?php include('inc/parts/tp-btn-1.php') ?>
 
 
-  </div><!-- END grid-container -->
+  <h3><?php //echo basename(__FILE__) ?></h3>
+
+  <?php include 'parts/tp-vid-thumb.php'; ?>
 
 
-</section><!-- END video-grid -->
+  <section class="video-grid">
+
+    <div class="grid-container">
+
+      <?php
+      // Mockup 4 dummy vid posts
+      for ($i = 0; $i < 5; $i++) {
+        include 'parts/tp-vid-thumb.php';
+      }
+      ?>
+
+
+    </div><!-- END grid-container -->
+
+
+  </section><!-- END video-grid -->
+
+</aside><!-- END sidebar -->
