d4bf4c16087a9e88848ba2a34fb2b411b3e7dc49
Eric L. Hepperle <codewizard13@gmail.com>
2025-02-10 07:40:35 -0600

    UPDATE init.php:  FINALIZE theme color vars

--- init.php
+++ init.php
@@ -1,27 +1,28 @@
 <?php
 // INITIALIZE VARIABLES
 
-require_once( 'functions.php');
+require_once('functions.php');
 
 define('SITE_TITLE', "ElijahStreams Rebuild 2025");
 
 $dummy_vid_title = "Johnny Enlow Unfiltered 138";
 
 // VERSION
-define('VERSION_NUM', '00.01.07' );
+define('VERSION_NUM', '00.01.07');
 
 
-  /* COLORS */
+/* COLORS */
 
 $theme_colors = [
   'oxford_blue' => ($oxford_blue = '#0E1C36'),
   'robin_egg_blue' => ($robin_egg_blue = '#00CAD0'),
-  'onyx' => '#373943',
-  'amethyst' => '#9a6fd7',
-  'seasalt' => '#f9fbfe',
+  'onyx' => ($onyx = '#373943'),
+  'amethyst' => ($amethyst = '#9a6fd7'),
+  'seasalt' => ($seasalt = '#f9fbfe'),
   'primary' => 'teal',
   'secondary' => $oxford_blue,
   
+
 ];
 
 
